fa3e2647376663844b7c45983c3f614ee90cd6ea
yuri dandrea <dandreaayuri@gmail.com>
2021-03-02 01:55:06 +0100

    in svolgimento

--- js/main.js
+++ js/main.js
@@ -12,51 +12,94 @@
 // Al termine della partita il software deve comunicare il punteggio, 
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-// genero un array estraendo 16 numeri casuali 
-var num = [];
-var mine =[];
-var numRimossi = [];
 
+// genero i 100 numeri 
+var num = [];
 
 for (var i = 0; i < 100; i++) {
   num[i] = i + 1;
-  while(mine.length < 16){
-    var r = Math.floor(Math.random() * 100) + 1;
-     numRimossi = removeItem(num, mine[r]); 
-     i++;
-  }
+}
+// genero mine 
+var mine = [];
+
+for (let i = 0; i < 16; i++) {
+  var n =  Math.floor(Math.random() * 100) + 1;
+  var check = mine.includes(n); //controllo che il numero non si ripeta
+
+  if(check === false) { // definisco come non far ripetere il numero
+    mine.push(n);
+  } else {
+    while(check === true) {
+      n = Math.floor(Math.random() * 100) + 1;
+      check = mine.includes(n);
+        if(check === false){
+          mine.push(n);
+        }
+      }
+    }
+  // const index = mine.indexOf(mine[n]); //tolgo i numeri dall'array totale (num) - non funziona
+  // if (index > -1) {
+  // num.splice(index, 16);
+  // }
 }
 console.log(mine);
 console.log(num);
 
-
-function removeItem(arr, value) { 
-  var index = arr.indexOf(value);
-  if (index > -1) {
-      arr.splice(index, 1);
-  }
-  return arr;
-}
-
 // chiedo all utente di inesire numeri tra 1 e 100 non puo inserire piu
 // volte lo stesso numero
-// var numUtente = [];
-// var numScelti = [];
+var sceltaUtente = [];
 
-// var i = 0;
-// while (numUtente[i] != numScelti){
-//   numUtente[i] = i + parseInt(prompt("nuemri"));
-//   numScelti.push(numUtente[i]);
-//   i++;
+// for (var i = 0; i < 100; i++) {
+//   var numUtente = parseInt(prompt("numero"));
+//   var check = sceltaUtente.includes(mine);
+//   if (check == false) {
+//     sceltaUtente.push(numUtente);
+//   } else { 
+//     prompt("boom");
+    
+//   }
 // }
-// console.log(numScelti);
-// console.log(numUtente);
-// se il numero e' uno dei 16 la partita termina
-// altrimenti si va avanti
+
+var numUtente = parseInt(prompt("numero"));
+var check = sceltaUtente.includes(mine);
+
+var x = 0;
+while (x < 10) {
+  numUtente = parseInt(prompt("numero"));
+  if (check == false) {
+    sceltaUtente.push(numUtente);
+    console.log(sceltaUtente);
+  } else {
+    alert("boom");
+  location.reload();
+  }
+  x++;
+}
+
+
+
+
+// var x = 0;
+//  while (x < 100) {
+//   var sceltaUtente = parseInt(prompt("numero"));
+//   if (sceltaUtente != mine) {
+//     alert("sei ancora vivo");
+//   } else if (sceltaUtente == mine){
+//     alert("BOOOM!");
+//     break;
+//   }
+//   x++;
+// }
 
 
 
 // software conta i punt
 
 
-if(mine.indexOf(r) === -1) mine.push(r);
+// while (check == true) {
+//   numUtente = parseInt(prompt("numero"));
+//   check = sceltaUtente.includes(numUtente);
+//   if (check == false) {
+//   sceltaUtente.push(numUtente);
+//   }
+// }
